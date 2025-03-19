611dc64fdd924ccadec3e616a5b0ab4543efac92
kekelaf08 <lafkeke473@gmail.com>
2025-03-19 10:20:03 -0400

    Je viens first.jsx et modifier App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Greeting from './Greeting';
+import First from './First';
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
       <Greeting message="Hello from the Greeting component!" />
+      <First message = "Hello from the First component!"/>
     </div>
   );
 }
---
+++ src/First.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+function First(props) {
+    return <p>Voici notre projet</p>;
+  }
+  export default First;
