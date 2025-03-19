98b40241f50746358d04a6bfa9b83672422a661c
kekelaf08 <lafkeke473@gmail.com>
2025-03-19 09:28:19 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeting from './Greeting';
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Greeting message="Hello from the Greeting component!" />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Greeting.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Greeting(props) {
+  return <h2>Voici notre projet</h2>;
+}
+
+export default Greeting;
